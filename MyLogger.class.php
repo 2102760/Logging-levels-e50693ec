35eae7c325517550db6aff574b498afe8f1c42c5
BeauvR <beauvrmc140@gmail.com>
2020-05-12 16:05:38 +0200

    uitwerking

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,8 +1,38 @@
 <?php
     class MyLogger
     {
-        public function log($message) {
-            error_log($message, 0);
+        public function log($message, $type) {
+            switch ($type) {
+                case 'error':
+                    $this->error($message);
+                    break;
+                case 'debug':
+                    $this->debug($message);
+                case 'notice' || 'info':
+                    $this->notice($message);
+                    break;
+                case 'warning':
+                    $this->warning($message);
+                    break;
+                default:
+                    error_log('unknown log type');
+            }
+        }
+
+        public function error($message) {
+            error_log('Error: ' . $message);
+        }
+
+        public function debug($message) {
+            error_log('Debug: ' . $message);
+        }
+
+        public function notice($message) {
+            error_log('Notice: ' . $message);
+        }
+
+        public function warning($message) {
+            error_log('Warning: ' . $message);
         }
     }
 ?>
